c12e1ac5b54abff6368a3a28a86d82daa85ab0ae
hamza nait <hamza2nait@gmail.com>
2024-07-12 14:35:32 +0100

    chore: Update file associations for C++ headers in VSCode settings

--- day06/ex00/ScalarConverter.cpp
+++ day06/ex00/ScalarConverter.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 #include <string>
 
-
 int ScalarConverter::getType(std::string str)
 {
 	if (str == "nan" || str == "nanf")
 		return (NN);
-	if (str == "inf" || str == "+inf" || str == "inff" || str == "+inff")
+	if (str == "+inf" || str == "+inff")
 		return (INF);
+	if (str == "-inf" || str == "-inff")
+		return (MINF);
 	if (str.length() == 1 && isalpha(str[0]))
 		return (CHAR);
-	if (str.find_first_not_of("0123456789") == std::string::npos)
+	if (str.find_first_not_of("0123456789") == std::string::npos &&
+		static_cast<long long>(std::strtoll(str.c_str(), nullptr, 10)) <= std::numeric_limits<int>::max() &&
+		static_cast<long long>(std::strtoll(str.c_str(), nullptr, 10)) >= std::numeric_limits<int>::min())
 		return (INT);
 	if (str.find_first_not_of("0123456789.") == std::string::npos &&
-		str.find_first_of(".") != std::string::npos &&
-		str.find_first_of(".") == str.find_last_of("."))
+		((str.find_first_of(".") != std::string::npos &&
+		  str.find_first_of(".") == str.find_last_of(".") &&
+		  str.find_first_of(".") != str.length() - 1) ||
+		 (str.find_first_of(".") == std::string::npos)))
 		return (DOUBLE);
-	if (str.find_first_not_of("0123456789.") == std::string::npos &&
-		str.find_first_of(".") != std::string::npos &&
-		str.find_first_of(".") != str.find_last_of(".") &&
+	if (str.find_first_not_of("0123456789.f") == std::string::npos &&
+		((str.find_first_of(".") != std::string::npos &&
+		  str.find_first_of(".") == str.find_last_of(".") &&
+		  str.find_first_of(".") != str.length() - 2) ||
+		 (str.find_first_of(".") == std::string::npos)) &&
 		str.find_first_of("f") == str.find_last_of("f") &&
 		str.find_first_of("f") == str.length() - 1)
 		return (FLOAT);
@@ -87,24 +94,60 @@ void ScalarConverter::printNan()
 void ScalarConverter::printInf()
 {
 	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: inff" << std::endl;
-	std::cout << "double: inf" << std::endl;
+	std::cout << "int: +inf" << std::endl;
+	std::cout << "float: +inff" << std::endl;
+	std::cout << "double: +inf" << std::endl;
+}
+
+void ScalarConverter::printMinf()
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: -inf" << std::endl;
+	std::cout << "float: -inff" << std::endl;
+	std::cout << "double: -inf" << std::endl;
 }
 
 void ScalarConverter::convert(std::string str)
 {
-	int type = getType(str);
+	int type;
+	char *end;
+	int precision = 0;
+	int sign = 1;
+
+	if (str.find_first_of("-") != std::string::npos &&
+		str.find_first_of("-") == str.find_last_of("-") &&
+		str.find_first_of("-") == 0 &&
+		str != "-inf" && str != "-inff")
+	{
+		str.erase(str.find_first_of("-"), 1);
+		sign = -1;
+	}
+	type = getType(str);
+	if (str.find_first_of(".") != std::string::npos)
+	{
+		for (size_t i = str.find_first_of("."); i < str.length(); i++)
+		{
+			if (str[i] != '0')
+			{
+				precision = i - str.find_first_of(".");
+			}
+		}
+		if (!precision)
+			precision = 1;
+		std::cout << std::fixed << std::setprecision(precision);
+	}
 	if (type == CHAR)
 		printChar(str[0]);
 	else if (type == INT)
-		printInt(std::stoi(str));
+		printInt(std::strtod(str.c_str(), &end) * sign);
 	else if (type == FLOAT)
-		printFloat(std::stof(str));
+		printFloat(std::strtof(str.c_str(), &end) * sign);
 	else if (type == DOUBLE)
-		printDouble(std::stod(str));
+		printDouble(std::strtod(str.c_str(), &end) * sign);
 	else if (type == NN)
 		printNan();
-	else
+	else if (type == INF)
 		printInf();
+	else if (type == MINF)
+		printMinf();
 }
--- day06/ex00/ScalarConverter.hpp
+++ day06/ex00/ScalarConverter.hpp
@@ -1,34 +1,36 @@
 #ifndef SCALARCONVERTER_HPP
-# define SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
-# include <iostream>
-# include <string>
-# include <cmath>
-# include <iomanip>
-# include <limits>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <limits>
 
-# define CHAR 1
-# define INT 2
-# define FLOAT 3
-# define DOUBLE 4
-# define INF 5
-# define NN 6
+#define CHAR 1
+#define INT 2
+#define FLOAT 3
+#define DOUBLE 4
+#define INF 5
+#define MINF 7
+#define NN 6
 
 class ScalarConverter
 {
-	private:
-		ScalarConverter();
-		ScalarConverter(const ScalarConverter &other);
-		static int		getType(std::string str);
-		static void		printChar(char c);
-		static void		printInt(int i);
-		static void		printFloat(float f);
-		static void		printDouble(double d);
-		static void		printNan();
-		static void		printInf();
-	
-	public:
-		static void		convert(std::string str);
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	static int getType(std::string str);
+	static void printChar(char c);
+	static void printInt(int i);
+	static void printFloat(float f);
+	static void printDouble(double d);
+	static void printNan();
+	static void printInf();
+	static void printMinf();
+
+public:
+	static void convert(std::string str);
 };
 
 #endif
